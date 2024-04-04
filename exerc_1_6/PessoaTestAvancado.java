141ec4a33af9fbae86174357173121d9a0de9f36
Deb4cker <ndebacher@gmail.com>
2024-04-04 16:44:00 -0300

    simplificação de testes exercicio 1.6; testes avançados

--- exerc_1_6/PessoaTestAvancado.java
+++ exerc_1_6/PessoaTestAvancado.java
@@ -3,8 +3,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class PessoaTest
+public class PessoaTestAvancado
 {
+     @Test
+    public void deveCriarPessoa(){
+        criarEChecarPessoa(2004);
+    }
+
     @Test
     public void deveCumprirEnunciadoDoExercicio(){
         String nome1 = "Maria";
@@ -17,8 +22,8 @@ public class PessoaTest
         double altura2 = 1.85;
         int anoNascimento2 = 1985;
 
-        Pessoa maria = new Pessoa(anoNascimento1);
-        Pessoa jose = new Pessoa(anoNascimento2);
+        Pessoa maria = criarEChecarPessoa(anoNascimento1);
+        Pessoa jose = criarEChecarPessoa(anoNascimento2);
 
         maria.setNome(nome1);
         maria.setPeso(peso1);
@@ -31,27 +36,32 @@ public class PessoaTest
         boolean casou = maria.casar(jose);
         boolean casouComAlguemCasado = jose.casar(maria);
 
-        assertEquals(true, casou);
-        assertEquals(false, casouComAlguemCasado);
-        assertEquals(jose.getNome(), maria.getNomeConjuge());
-        assertEquals(maria.getNome(), jose.getNomeConjuge());
+        assertTrue(casou, "O casamento entre duas pessoas não casadas pode acontecer.");
+        assertFalse(casouComAlguemCasado, "O casamento não deve ocorrer para uma pessoa casada.");
+
+        assertTrue(maria.ehCasado(), "Maria deve estar casada.");
+        assertTrue(jose.ehCasado(), "José deve estar casado.");
+
+        assertEquals(jose.getNome(), maria.getNomeConjuge(), "O nome do conjuge de Maria deve ser José");
+        assertEquals(maria.getNome(), jose.getNomeConjuge(), "O nome do conjuge de José deve ser Maria");
     }
 
     @Test
     public void dadosDaPessoaDevemSerOsMesmosInformados(){
+
         String nome = "Maria";
         double peso = 56.8;
         double altura = 1.65;
 
-        Pessoa maria = new Pessoa(2002);
+        Pessoa maria = criarEChecarPessoa(2002);
 
         maria.setNome(nome);
         maria.setPeso(peso);
         maria.setAltura(altura);
 
-        assertEquals(nome, maria.getNome());
-        assertEquals(peso, maria.getPeso());
-        assertEquals(altura, maria.getAltura());
+        assertEquals(nome, maria.getNome(), "O nome da pessoa deve ser o mesmo informado.");
+        assertEquals(peso, maria.getPeso(), "O peso da pessoa deve ser o mesmo informado.");
+        assertEquals(altura, maria.getAltura(), "A altura da pessoa deve ser a mesmo informada.");
     }
 
     @Test
@@ -59,9 +69,8 @@ public class PessoaTest
         String nome = "Maria";
         double peso = 56.8;
         double altura = 1.65;
-        int anoNascimento = 2002;
 
-        Pessoa maria = new Pessoa(anoNascimento);
+        Pessoa maria = criarEChecarPessoa(2002);
 
         maria.setNome(nome);
         maria.setPeso(peso);
@@ -75,9 +84,13 @@ public class PessoaTest
         maria.setPeso(pesoInvalido);
         maria.setAltura(alturaInvalida);
 
-        assertEquals(nome, maria.getNome());
-        assertEquals(peso, maria.getPeso());
-        assertEquals(altura, maria.getAltura());
+        assertEquals(nome, maria.getNome(), "O nome da pessoa deve ser mantido.");
+        assertEquals(peso, maria.getPeso(), "O peso da pessoa deve ser mantido.");
+        assertEquals(altura, maria.getAltura(), "A altura da pessoa deve ser mantido.");
+
+        assertNotEquals(nomeInvalido, maria.getNome(), "O nome da pessoa não pode ser invalido.");
+        assertNotEquals(pesoInvalido, maria.getPeso(), "O peso da pessoa não pode ser invalido.");
+        assertNotEquals(alturaInvalida, maria.getAltura(), "A altura da pessoa não pode ser invalida.");
     }
 
     @Test
@@ -87,7 +100,7 @@ public class PessoaTest
         double altura = 1.65;
         int anoNascimento = 1990;
 
-        Pessoa maria = new Pessoa(anoNascimento);
+        Pessoa maria = criarEChecarPessoa(anoNascimento);
 
         maria.setNome(nome);
         maria.setPeso(peso);
@@ -101,5 +114,11 @@ public class PessoaTest
                 '}';
 
         assertEquals(formatoCorreto, maria.toString());
+    }
+
+    private Pessoa criarEChecarPessoa(int anoNascimento){
+        Pessoa pessoa = new Pessoa(anoNascimento);
+        assertNotNull(pessoa, "A pessoa não pode ser nula.");
+        return pessoa;
     }
 }
